771638b5ffaae50d3ad5158a2c5b86f169310ca3
Facundo Arca <farca9@gmail.com>
2019-11-14 08:40:55 -0300

    Updates

---
+++ precache-manifest.8d7ae82110d74517a362a9d2d6f1ecfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6667e63820df9e989a160ced64d09e70",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d0cea727aadaf60ab50f",
+    "url": "/monsters-rolodex/static/css/main.1edea56d.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "d0cea727aadaf60ab50f",
+    "url": "/monsters-rolodex/static/js/main.14138c45.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
