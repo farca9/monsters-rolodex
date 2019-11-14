9cc215a280a442f08ed1ae951b207411f18c2fe2
Facundo Arca <farca9@gmail.com>
2019-11-14 13:12:16 -0300

    Titulo

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: '',
+      title:''
     };
   }
 
@@ -20,23 +21,23 @@ class App extends Component {
   }
 
   handleChange = (e) => {
-    this.setState({searchField: e.target.value})
+    this.setState({searchField: e.target.value, title: event.target.value})
   }
 
   render(){
 
-    const { monsters, searchField } = this.state;
+    const { monsters, searchField, title } = this.state;
     const filteredMonsters = monsters.filter(
       monster => monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
     )
 
     return (
     <div className="App">
-      <h1>Monsters Rolodex</h1>
+      <h1>{title}</h1>
       <SearchBox 
       placeholder='Search monsters' 
       handleChange={this.handleChange}/>
-      <CardList monsters={filteredMonsters}/>
+      <CardList monsters={monsters}/>
     </div>
     );
   }
